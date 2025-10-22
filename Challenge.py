c5f4b8c13833ef38f544c1fd4434105119df8589
Lordaria <lordariaazumah@gmail.com>
2025-10-22 20:32:24 +0100

    Creation of Account

--- Challenge.py
+++ Challenge.py
@@ -1,1 +1,11 @@
-print("")
+print("---------WELCOME TO LORDISH BANKING SYSTEM----------")
+print("ACCOUNT CREATION")
+username = input("Choose a username: ")
+password = input("Enter a password: ")
+if len(password) < 7:
+    print("Password must be seven(7) charaters long!")
+    account_creation = False
+else:
+    confirm_password = input("Confirm your password: ")
+if password != confirm_password:
+    print("Password does not match.")
